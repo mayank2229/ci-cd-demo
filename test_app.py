bd44b33c00a9d9a910fddc086e036986c47532f5
Mayank Mishra <126587047+mayank2229@users.noreply.github.com>
2026-05-03 14:54:49 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -4,3 +4,4 @@ def test_home():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
+    assert b"Hello this is submitted by Mayank" in response.data
